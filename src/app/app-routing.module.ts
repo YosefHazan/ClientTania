7f2d2c4f5d8cf7331b75a4f3b30566c1c225a13e
‘YosefHazan‘ <yosef771@gmail.com>
2019-10-31 00:03:55 +0200

    router work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import {ShowLessonComponent} from '../app/show-lesson/show-lesson.component';
 import{MidrashaComponent} from '../app/midrasha/midrasha.component';
 const routes: Routes = [
-  { path: '**', redirectTo: 'chitat'},
- // { path:'',    redirectTo:'/ClientTania/chitat', pathMatch: 'full' },
+  /*{ path: '**', redirectTo: 'chitat'},
+  */
+  { path:'', redirectTo:'chitat', pathMatch: 'full' },
   { path:'chitat', component:ShowLessonComponent},
   { path:'midrasha', component:MidrashaComponent}
   //{ path:'login', component:LoginComponent},
