4947a5878ce77c2a9acb42ff9241c522c0449db1
YosefHazan <yosef771@gmail.com>
2020-12-08 01:22:46 +0200

    books and chitat/midrasha

--- app.component.ts
+++ app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, Input, Output} from '@angular/core';
 import { Router } from '@angular/router';
+import { BuyLecturesComponent} from './com/buy-lectures/buy-lectures.component';
+import {BuyBooksComponent} from './com/buy-Books/buy-Books.component';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public RouterbuyBooks:string = "/buyBooks";
-  public routerbuyLectures:string ="/buyLectures";
+  public RouterbuyBooks:boolean = false;
+  public RouterbuyLectures:boolean = false;
   constructor(){}
+  ManualRouterToBooks()
+  {
+    //in first time books and lacture is equal to false
+    //and they are showing together
+    //so i check if is first time then set only one in showing
+    if(this.RouterbuyLectures != this.RouterbuyBooks)
+    {
+      this.RouterbuyLectures = !this.RouterbuyLectures;
+    }
+    this.RouterbuyBooks = !this.RouterbuyBooks;
+  }
+  ManualRouterToLectures()
+  {
+    if(this.RouterbuyLectures != this.RouterbuyBooks)
+    {
+      this.RouterbuyLectures = !this.RouterbuyLectures;
+    }
+    this.RouterbuyBooks = !this.RouterbuyBooks;
+  }
+  TurenOffLectureOrBook()
+  {
+    console.log("turn off");
+    this.RouterbuyBooks = false;
+    this.RouterbuyLectures = false;
+  }
 }
