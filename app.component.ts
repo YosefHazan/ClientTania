a226654711c9cbf1c882f9086274d5b2ec360455
YosefHazan <yosef771@gmail.com>
2020-12-11 05:44:59 +0200

    books/lecture and chitat/midrasha monile and pc, need more responsive, but good for now

--- app.component.ts
+++ app.component.ts
@@ -11,33 +11,68 @@ import {BuyBooksComponent} from './com/buy-Books/buy-Books.component';
 export class AppComponent {
   public RouterbuyBooks:boolean = false;
   public RouterbuyLectures:boolean = false;
+  public TypeLessonsActived:string;
   constructor(){}
+  ChitatComponent()
+  {
+    this.TurenOffLectureOrBook();
+    this.TypeLessonsActived = 'chitat';
+  }
+  MidrashaComponent()
+  {
+    this.TurenOffLectureOrBook();
+    this.TypeLessonsActived = 'midrasha';
+  }
   ManualRouterToBooks()
   {
-    var elmToHide;
     //in first time books and lacture is equal to false
     //and they are showing together
     //so i check if is first time then set only one in showing
     this.RouterbuyLectures = false;
     this.RouterbuyBooks = true;
-    elmToHide = document.querySelector('[yyAttrr="descriptionArea"]');
-    elmToHide.style.display = 'none';
+    this.fixCssForSells();
   }
   ManualRouterToLectures()
   {
-    var elmToHide;
     this.RouterbuyLectures = true;
     this.RouterbuyBooks = false;
-    elmToHide = document.querySelector('[yyAttrr="descriptionArea"]');
-    elmToHide.style.display = 'none';
+    this.fixCssForSells();
   }
   TurenOffLectureOrBook()
   {
     var elmToHide;
+    var elmToDown;
+
     console.log("turn off");
     this.RouterbuyBooks = false;
     this.RouterbuyLectures = false;
     elmToHide = document.querySelector('[yyAttrr="descriptionArea"]');
     elmToHide.style.display = 'block';
+  
+    //fix position of sells description
+    elmToDown = document.querySelector('.yyRightDirection');
+    elmToDown.style.marginTop = "0px";
+  }
+  fixCssForSells()
+  {
+    var elmToHide;
+    var elmToDown;
+    var pixelsToDown;
+
+    //hide element in background
+    elmToHide = document.querySelector('[yyAttrr="descriptionArea"]');
+    elmToHide.style.display = 'none';
+    
+    //fix position of sells description
+    elmToDown = document.querySelector('.yyRightDirection');
+    if(screen.width <= 600 && this.TypeLessonsActived.includes("chitat"))
+    {
+      pixelsToDown = "378px";
+    }
+    else if(screen.width <= 600 && this.TypeLessonsActived.includes("midrasha"))
+    {
+      pixelsToDown = "598px";
+    }
+    elmToDown.style.marginTop = pixelsToDown;
   }
 }
