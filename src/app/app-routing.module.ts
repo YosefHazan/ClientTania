ccffc6906160b78bd1415092bb4b3fc05f34ca36
YosefHazan <yosef771@gmail.com>
2021-01-15 04:03:35 +0200

    css for home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-/*import { BuyBooksComponent }      from './com/buy-books/buy-books.component'
+import { NgModule }               from '@angular/core';
+import { Routes, RouterModule }   from '@angular/router';
+/*import { BuyBooksComponent }    from './com/buy-books/buy-books.component'
 import { BuyLecturesComponent }   from './com/buy-lectures/buy-lectures.component';*/
 import { ChitatComponent }        from './com/chitat/chitat.component';
 import { MidrashaComponent }      from './com/midrasha/midrasha.component';
+import { HomeCompsComponent }     from './com/subCom/home-comps/home-comps.component';
 
 const routes: Routes = [
-  { path: "",                pathMatch: "full" ,redirectTo: "chitat"},
-  { path: "chitat",          component:ChitatComponent},
-  { path: "midrasha",        component:MidrashaComponent}
+  { path: "",                pathMatch: "full", redirectTo: "Home"},
+  { path: "chitat",          component: ChitatComponent },
+  { path: "midrasha",        component: MidrashaComponent },
+  { path: "Home",            component: HomeCompsComponent }
   /*{ path: "buyLectures",     component:BuyLecturesComponent},
   { path: "buyBooks" ,       component:BuyBooksComponent}*/
 ];
