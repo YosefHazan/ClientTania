ac6047da99541e282181e968e61da702eeafc99a
YosefHazan <yosef771@gmail.com>
2021-05-02 02:07:38 +0300

    finish font and example chumash

--- src/app/com/example-midrasha/example-midrasha.component.ts
+++ src/app/com/example-midrasha/example-midrasha.component.ts
@@ -10,23 +10,14 @@ import { GetAllCategoryLessonService } from '../../services/get-all-category-les
 })
 
 export class ExampleMidrashaComponent implements OnInit {
+  public yyIsTuchHere:boolean[] = [];
+  public callObservable: any;
+  public AllLessons: any;
+  public yyaudio:HTMLAudioElement;
+  public getSrcAudio:string = "//tanyayomi.com/%D7%A4%D7%A8%D7%A9%D7%AA%20%D7%90%D7%97%D7%A8%D7%99%20%D7%9E%D7%95%D7%AA-%D7%A7%D7%93%D7%95%D7%A9%D7%99%D7%9D%20-%20%D7%902291.m4a";
+  public SaveUserChoiseDate:number = 0;
 
-  public callObservable:any;
-  public TablesName:minLessons[] = [];
-  public HelpTablesName:minLessons;
-  public AllLessons:any;
-  public AllTables:any; 
-  public nameToReq:any;
-  public Lessons:boolean;
-  public result:minLessons[] = [];
-  public TablesNameObservable : any;
-  private glObj:minLessons;
-  
-  renderer: any;
-  
-  constructor(private lessonsService: CallToMidrashaService
-              ,private tableService: GetAllCategoryLessonService
-              ,private yytHtmlElm:ElementRef) {}
+  constructor(private yytHtmlElm:ElementRef) {}
   /*
   ngAfterViewInit() {
     const d2 = this.renderer.createElement('div');
@@ -39,25 +30,20 @@ export class ExampleMidrashaComponent implements OnInit {
   playMidrasha(){
     console.log("playMidrasha");
     //this.elRef.nativeElement.querySelector('div');
-    let chtchTableParasha = this.renderer.selectRootElement("#playMidrasha");
-    console.log(chtchTableParasha.length);
+    this.yytHtmlElm.nativeElement.querySelector("#tableParasha").style.display = 'none';
+    this.yytHtmlElm.nativeElement.querySelector("#descriptionLesson").style.display = 'block';
+    //
   }
-  
+  toggleDisplay(){
+    //remuve old rowClicked
+    if(document.querySelector(".activRow"))
+    { 
+      document.querySelector(".activRow").classList.remove("activRow");
+    }
+    document.querySelector("#" + 'id1').parentElement.classList.add("activRow");
+  }
   ngOnInit()
   {
-    let elm;
-
-    this.TablesNameObservable = this.tableService.getCategorysName().subscribe(data => {
-      this.AllTables = data;
-      if (this.AllTables == ""){console.log("TableEmpty");}
-      for (let index = 0; index < this.AllTables.length; index++) {
-        elm = this.AllTables[index];
-        this.HelpTablesName = new minLessons();
-        this.HelpTablesName["Name"] = elm.split(":")[1];
-        this.HelpTablesName["Url"] = elm.split(":")[0];
-        this.TablesName.push(this.HelpTablesName);
-      }
-    });
   }
   /*
   onKey(event: any){
@@ -65,54 +51,7 @@ export class ExampleMidrashaComponent implements OnInit {
     this.Lessons = this.getLessons(this.nameToReq);
   }
   */
-  yyCalToMidrashaByTable(yyid:any, TablesName:string){
-      //remuve old rowClicked
-      if(document.querySelector(".activRow"))
-      { 
-        document.querySelector(".activRow").classList.remove("activRow");
-      }
-      document.querySelector("#" + yyid).parentElement.classList.add("activRow");
-
-
-   //get all lessons exist in midrasha
-    this.callObservable = this.lessonsService.getAllLessons(TablesName).subscribe(data=>{
-      this.AllLessons = data;
-    });
-    if (this.AllLessons == ""){console.log("AllLessons - TableEmpty");}
-    //this.callObservable.unsubscribe();
-  }
-
-  getLessons(name1:string):boolean{ // this function is for sreach input
-    let isHaveResult;
-    console.log(name1 + " name1 " + this.AllLessons[1]);
-    
-    //get list of all currect result
-    this.AllLessons.forEach(element => {
-      console.log(element['title'] + " " + name1 + " + " + element['title'].startsWith(name1));
-      if(element['title'].startsWith(name1) && (name1 != "") && (name1 != " ")){
-        isHaveResult = true;
-
-        this.glObj = {
-          Name:element['title'],
-          Url:element['link'],
-          Type:element['type']
-        }
-        
-        console.log(this.glObj.Url +" : "+element['title']);
-        this.result.push(this.glObj);
-      }
-      else{
-        isHaveResult = false;
-      }
-    });
-      return isHaveResult; 
-  }
-  getTablesName(){
-  
-  }
   
   ngOnDestroy(){
-    //this.callObservable.unsubscribe();
-    this.TablesNameObservable.unsubscribe();
   }
 }
